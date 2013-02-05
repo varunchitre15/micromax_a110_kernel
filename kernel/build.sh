ebf0212a375136bfc06a7a59f758a67d8a2c1ce6
Varun <varun.chitre15@gmail.com>
2013-02-05 13:12:39 +0000

    Added build script, bootimg make and flashable zip maker

--- kernel/build.sh
+++ kernel/build.sh
@@ -1,2 +1,5 @@
 make TARGET_PRODUCT=s9081 MTK_ROOT_CUSTOM=../mediatek/custom CROSS_COMPILE=/home/varun.chitre15/A70_kernel/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+echo -e "Making boot.img"
 ./repack.pl -boot arch/arm/boot/zImage ramdisk boot.img
+echo -e "Making flashable zip"
+zip -r kernelflasher META-INF boot.img
---
+++ kernel/build2.sh
@@ -0,0 +1,2 @@
+make TARGET_PRODUCT=s9081 MTK_ROOT_CUSTOM=mediatek/custom CROSS_COMPILE=/home/varun.chitre15/A70_kernel/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+./repack.pl -boot arch/arm/boot/zImage ramdisk boot.img
